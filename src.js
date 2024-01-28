59abd5bdc361cb0c739fb581321ea0b73e894672
viraj-rajage <virajrajage17@gmail.com>
2024-01-28 20:37:22 +0530

    app hoster

--- src.js
+++ src.js
@@ -1,51 +1,20 @@
-// const icon = document.getElementById('menu-icon');
-// const hiddenDiv = document.getElementById('menu-items');
+
 
 const menuIcon = document.getElementById('menu-icon');
 const menuItems = document.getElementById('menu-items');
 
-
-// icon.addEventListener('mouseenter', () => {
-//   hiddenDiv.style.display = 'block';
-// });
-
-// icon.addEventListener('click', () => {
-//   if (!event.target.matches('#menu-icon') && !event.target.closest('.menu-items')) {
-//     menuItems.style.display = 'none';
-//   }
-// });
-
-
-// // function hideMenu() {
-// //   hiddenDiv.style.display = 'none';
-// // }
-
-// window.addEventListener('resize', () => {
-//   if (window.innerWidth <= 930) {
-//       hideMenu();
-//   } else {
-//       hiddenDiv.style.display = 'block';
-//   }
-// });
-
-// // JavaScript to hide menu when screen width is 930px or less
-// function hideMenu() {
-//   hiddenDiv.style.display = 'none';
-// }
 
 
 menuIcon.addEventListener('click', () => {
   menuItems.style.display = (menuItems.style.display === 'none' || menuItems.style.display === '') ? 'block' : 'none';
 });
 
-// Close the menu if clicked outside or on a menu item
 document.addEventListener('click', (event) => {
   if (!event.target.matches('#menu-icon') && !event.target.closest('.menu-items')) {
       hideMenu();
   }
 });
 
-// Adjust visibility on window resize
 window.addEventListener('resize', () => {
   if (window.innerWidth <= 930) {
       hideMenu();
@@ -54,7 +23,6 @@ window.addEventListener('resize', () => {
   }
 });
 
-// JavaScript to hide menu when screen width is 930px or less
 function hideMenu() {
   menuItems.style.display = 'none';
 }
@@ -65,3 +33,6 @@ document.addEventListener('contextmenu', function (e) {
   });
 
 
+  setTimeout(function() {
+    location.reload();
+}, 5000);
